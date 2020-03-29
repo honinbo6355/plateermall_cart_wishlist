778ff150e23af6324d6731dfe257b7581bf009f8
Songseail <seail379@gmail.com>
2020-03-30 00:36:58 +0900

    wishlist api 시작

--- shoppingmall_cart_wishlist_rest/src/main/java/com/plateer/WishlistController.java
+++ shoppingmall_cart_wishlist_rest/src/main/java/com/plateer/WishlistController.java
@@ -1,10 +1,30 @@
 package com.plateer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
+@CrossOrigin(allowCredentials = "true", origins = {"*"}, methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT},
+        allowedHeaders = {"Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                "Access-Control-Request-Headers", "Access-Control-Allow-Origin", "Set-Cookie"},
+        exposedHeaders = {"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"}, maxAge = 3000)
 @RequestMapping("/api/wishlist")
 public class WishlistController {
 
+	@GetMapping("")
+	public List<String> getGoodsCodes() {
+		List<String> goodsCodes = new ArrayList<>();
+		goodsCodes.add("1203917700");
+		
+		return goodsCodes;
+	}
 }
